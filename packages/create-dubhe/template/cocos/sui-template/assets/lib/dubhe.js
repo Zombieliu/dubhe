4b1c9886c7a9724fff55f943fcf36c123384e05d
vladilen11 <vladilenaksana@163.com>
2024-11-25 17:14:54 +0800

    hotfix: update create-dubhe sui-client

--- packages/create-dubhe/template/cocos/sui-template/assets/lib/dubhe.js
+++ packages/create-dubhe/template/cocos/sui-template/assets/lib/dubhe.js
@@ -4562,6 +4562,26 @@ function numberToAddressHex(num) {
 
 // src/dubhe.ts
 
+// src/errors/index.ts
+var ContractDataParsingError = class extends Error {
+  constructor(dryResult) {
+    const error = dryResult?.effects?.status?.error || "";
+    const functionMatch = error ? error.match(/function_name: Some\("([^"]+)"\)/) : null;
+    const moduleMatch = error ? error.match(/address: ([a-fA-F0-9]+)/) : null;
+    const functionName = functionMatch ? functionMatch[1] : "unknown";
+    const moduleAddress = moduleMatch ? "0x" + moduleMatch[1] : "unknown";
+    const errorMessage = dryResult.error ? dryResult.error : "UNKNOWN_ERROR";
+    const message = [`
+- Function: ${functionName}`, `- Module Address: ${moduleAddress}`, `- Error Message: ${errorMessage}`].join("\n");
+    super(message);
+    this.errorType = "ContractDataParsingError";
+    this.functionName = functionName;
+    this.moduleAddress = moduleAddress;
+    this.errorMessage = errorMessage;
+  }
+};
+
+// src/dubhe.ts
 function isUndefined(value) {
   return value === void 0;
 }
@@ -4953,7 +4973,7 @@ var Dubhe = class {
       }
       return returnValues;
     } else {
-      return void 0;
+      throw new ContractDataParsingError(dryResult);
     }
   }
   /**
